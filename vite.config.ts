9a04415e260bc0bd17970f2b72f6760c02325bb4
Tejal Desai <tejal29@gmail.com>
2025-10-16 12:34:53 -0700

    fix for javascriptor wasm module

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,11 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
   },
   server: {
     port: 3000,
